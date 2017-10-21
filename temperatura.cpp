2e9134abc311245f95173fa64aad9762b66c9a7c
Nesi73 <32869715+nesi73@users.noreply.github.com>
2017-10-21 10:57:34 +0200

    Create temperatura.cpp

---
+++ temperatura.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	const float F1=32;     /* Constante */
+	const float F2=180;    /* Constante */
+	const float C1=100;    /* Constante */
+	float C, F;            /* Variable  */ 
+	
+	cout << "Introduce una temperatura en grados centigrados: ";
+	cin >> C;
+	F=C/100*180+32;
+	cout << "La temperatura en grados fahrenheit es ";
+	cout << F << endl;
+	
+	system("PAUSE");
+	return 0;
+}
